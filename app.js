2664cd482576d83013f00ca789f33f5384fe7490
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-07 19:34:36 +0200

    moved stories array to service and use dependency injection in main controller

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 var myModule = angular.module('Angello', []);
 myModule.factory('AngelloHelper', function() { });
-myModule.service('AngelloModel', function() { });
-myModule.controller('MainCtrl', function() {
+myModule.service('AngelloModel', function() {
+  var service = this,
+    stories = [
+      {
+        title: "First story",
+        description: "Our first story.",
+        criteria: "Criteria pending.",
+        status: "To Do",
+        type: "Feature",
+        reporter: "Lukas Ruebbelke",
+        assignee: "Brian Ford"
+      },
+      {
+        title: "Second story",
+        description: "Do something.",
+        criteria: "Criteria pending.",
+        status: "Back Log",
+        type: "Feature",
+        reporter: "Lukas Ruebbelke",
+        assignee: "Brian Ford"
+      },
+      {
+        title: "Another story",
+        description: "Just one more.",
+        criteria: "Criteria pending.",
+        status: "Code Review",
+        type: "Enhancement",
+        reporter: "Lukas Ruebbelke",
+        assignee: "Brian Ford"
+      }
+    ];
+
+    service.getStories = function() {
+      return stories;
+    };
+});
+
+myModule.controller('MainCtrl', function(AngelloModel) {
   var main = this;
 
-  main.stories = [
-    {
-      title: "First story",
-      description: "Our first story.",
-      criteria: "Criteria pending.",
-      status: "To Do",
-      type: "Feature",
-      reporter: "Lukas Ruebbelke",
-      assignee: "Brian Ford"
-    },
-    {
-      title: "Second story",
-      description: "Do something.",
-      criteria: "Criteria pending.",
-      status: "Back Log",
-      type: "Feature",
-      reporter: "Lukas Ruebbelke",
-      assignee: "Brian Ford"
-    },
-    {
-      title: "Another story",
-      description: "Just one more.",
-      criteria: "Criteria pending.",
-      status: "Code Review",
-      type: "Enhancement",
-      reporter: "Lukas Ruebbelke",
-      assignee: "Brian Ford"
-    }
-  ];
+  main.stories = AngelloModel.getStories();
 
   main.createStory = function() {
     main.stories.push({
