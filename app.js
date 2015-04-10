490bb04b2249ca0b818025911305a112e7163a57
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-10 20:55:16 +0200

    add buildIndex helper method

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var myModule = angular.module('Angello', []);
-myModule.factory('AngelloHelper', function() { });
+myModule.factory('AngelloHelper', function() {
+  var buildIndex = function(source, property) {
+    var tempArray = [];
+
+    for(var i = 0, len = source.length; i < len; ++i) {
+      tempArray[source[i][property]] = source[i];
+    }
+
+    return tempArray;
+  };
+
+  return {
+    buildIndex:  buildIndex
+  };
+});
+
 myModule.service('AngelloModel', function() {
   var service = this,
     stories = [
